7201fcf48aae97b9a290bdb8d6f3e7d8d7a1af41
radialmonster <radial@gmail.com>
2025-10-15 22:56:07 -0400

    fix: Get block icons from the client-side

--- wp-blocks-to-category.php
+++ wp-blocks-to-category.php
@@ -138,7 +138,7 @@ class WP_Blocks_To_Category {
         wp_enqueue_script(
             'wpbtc-admin-script',
             WPBTC_PLUGIN_URL . 'assets/js/admin.js',
-            array('jquery'),
+            array('jquery', 'wp-blocks'),
             WPBTC_VERSION,
             true
         );
@@ -170,25 +170,10 @@ class WP_Blocks_To_Category {
         $blocks_for_page = array();
 
         foreach ($registered_blocks as $block_name => $block_type) {
-            if ($block_name === 'core/archives') {
-                echo '<pre>';
-                print_r($block_type);
-                echo '</pre>';
-            }
-
-            $icon = 'dashicons-block-default';
-            if (isset($block_type->icon)) {
-                if (is_string($block_type->icon)) {
-                    $icon = $block_type->icon;
-                } elseif (is_array($block_type->icon) && isset($block_type->icon['src'])) {
-                    $icon = $block_type->icon['src'];
-                }
-            }
-
             $blocks_for_page[] = array(
                 'name' => $block_name,
                 'title' => isset($block_type->title) ? $block_type->title : $block_name,
-                'icon' => $icon
+                'icon' => isset($block_type->icon) ? $block_type->icon : 'dashicons-block-default'
             );
         }
 
